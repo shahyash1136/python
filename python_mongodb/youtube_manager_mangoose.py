77792b7e9cac4d363e3da5e451fa88d8e2d734ba
shahyash1136 <43175218+shahyash1136@users.noreply.github.com>
2024-03-19 21:09:18 +0530

    completed python core

---
+++ python_mongodb/youtube_manager_mangoose.py
@@ -0,0 +1,74 @@
+
+from pymongo import MongoClient
+from bson import ObjectId
+
+client = MongoClient(
+    "mongodb+srv://test:test@cluster0.juwgiuq.mongodb.net/", tlsAllowInvalidCertificates=True)
+
+db = client['ytmanager']
+
+video_collection = db['videos']
+
+# print(video_collection)
+
+
+def list_all_videos():
+    print("\n")
+    print('*'*70)
+    for video in video_collection.find():
+        id = video['_id']
+        name = video['name']
+        time = video['time']
+        print(f"ID: {id}, Name: {name}, Time: {time}")
+    print("\n")
+    print('*'*70)
+
+
+def add_video(name, time):
+    video_collection.insert_one({"name": name, "time": time})
+
+
+def update_video(video_id, name, time):
+    video_collection.update_one(
+        {'_id': ObjectId(video_id)}, {"$set": {"name": name, "time": time}})
+
+
+def delete_video(video_id):
+    video_collection.delete_one({'_id': ObjectId(video_id)})
+
+
+def main():
+    while True:
+        print("\n Youtube manager app with DB")
+        print("1. List all youtube videos ")
+        print("2. Add a youtube video ")
+        print("3. Update a youtube video details ")
+        print("4. Delete a youtube video ")
+        print("5. Exit the app ")
+        choice = input("Enter your choice: ")
+
+        match choice:
+            case '1':
+                list_all_videos()
+            case '2':
+                name = input("Enter Video Name: ")
+                time = input("Enter Video Time: ")
+                add_video(name, time)
+            case '3':
+                list_all_videos()
+                video_id = input("Enter Video ID to update: ")
+                name = input("Enter Video Name: ")
+                time = input("Enter Video Time: ")
+                update_video(video_id, name, time)
+            case '4':
+                list_all_videos()
+                video_id = input("Enter Video ID to delete: ")
+                delete_video(video_id)
+            case '5':
+                break
+            case _:
+                print("Invalid Choice")
+
+
+if __name__ == '__main__':
+    main()
