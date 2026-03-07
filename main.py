e9471846d054f5b0662858daf1e4ef5605fc7107
Yash Shah <43175218+shahyash1136@users.noreply.github.com>
2026-03-07 14:30:04 +0530

    completed variables

---
+++ main.py
@@ -0,0 +1,3 @@
+# This is my first python program
+print("Hello World!")
+print("It's really good!")
---
+++ variables/variable.py
@@ -0,0 +1,34 @@
+# Variable = A container for a value (string, integer, float, boolean)
+#           A variable behaves as if it was the value it contains
+
+# Strings
+first_name = "Yash Shah"
+food = "pizza"
+email = "yash.shah@gamil.com"
+
+# print(f"Hello {first_name}")
+# print(f"You like {food}")
+# print(f"Your email is {email}")
+
+# Integers
+age = 32
+quantity = 3
+num_of_students = 30
+
+# print(f"You are {age} years old")
+# print(f"You are buying {quantity} items")
+# print(f"You class has {num_of_students} students")
+
+# Float
+price = 10.99
+gpa = 3.2
+distance = 5.5
+
+# print(f"The price is ${price}")
+# print(f"Your gpa is: {gpa}")
+# print(f"You ran {distance}km")
+
+# Boolean
+is_student = True
+
+# print(f"Are you a student?: {is_student}")
