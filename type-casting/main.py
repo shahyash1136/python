e1d731f95783eab40bf399dce2ea414622e14f6e
Yash Shah <43175218+shahyash1136@users.noreply.github.com>
2026-03-07 14:46:06 +0530

    completed Typecasting

---
+++ type-casting/main.py
@@ -0,0 +1,16 @@
+# Typecasting = the process of converting a variable from one data type to another
+#               str(), int(), float(), bool()
+
+name = "Yash Shah"
+age = 25
+gpa = 3.6
+is_student = True
+
+gpa = int(gpa)
+# age = float(age)
+age = str(age)
+name = bool(name)
+
+print(gpa)
+print(age)
+print(name)
